1c1e916b0d07536fcc8ecf6ba2dc021588c4b6d3
Mark Kevin Bilsland Marchesan <markkevin@Marks-MacBook-Air-2.local>
2024-10-16 18:14:06 -0300

    First github push  :D

---
+++ bash_script.sh
@@ -0,0 +1,16 @@
+# Comando que mostre a pasta/diretório onde você está
+printf "\033[0;34mDiretório:	\033[0m\n"	
+pwd
+
+# Comando que gere uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
+printf "\033[0;34mLista detalhada do diretório 'Home':	\033[0m\n"
+ls -l ~	
+printf "\033[0;34m...Copiando lista detalhada do diretório 'Home' para o arquivo 'lista_HOME.txt'...\033[0m\n"
+ls -l ~ > lista_HOME.txt
+
+# Comando que mostre a pasta/diretório onde você está
+printf "\033[0;34mData e tempo:	\033[0m\n"
+date
+
+
+/Users/markkevin/CEN0336_12532865_P1/bash_script.sh
